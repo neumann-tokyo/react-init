bf7f6e3c70b5827e0b333da55de3bffbd28b8604
kbaba1001 <kbaba1001@users.noreply.github.com>
2024-08-08 05:32:05 +0000

    feat: add devcontainer

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,10 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	server: {
+		host: "0.0.0.0",
+		port: 5173,
+	},
 	test: {
 		environment: "happy-dom",
 	},
